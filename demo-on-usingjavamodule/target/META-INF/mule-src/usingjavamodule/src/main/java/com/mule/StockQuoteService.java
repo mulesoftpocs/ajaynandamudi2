e5038dcb0165f98bf5dc3667f236b75504ee8f3a
Ajay Kumar Nandamudi <anandamudi@corp.miraclesoft.com>
2021-01-21 18:15:12 +0530

    commit

---
+++ demo-on-usingjavamodule/target/META-INF/mule-src/usingjavamodule/src/main/java/com/mule/StockQuoteService.java
@@ -0,0 +1,20 @@
+package com.mule;
+
+public class StockQuoteService {
+	
+	public static String sayHello(String name,String country) {
+		return "Hello "+name +"  from "+country;
+	}
+
+	
+	public String getStockPrice(String stockSymbol) {
+		
+		if(stockSymbol.equals("Microsoft")){
+			return "100";
+	   }else if(stockSymbol.equals("Google")){
+			return "200";
+	   }else {
+			return "stock doesn't exists";
+		}
+	}
+}
